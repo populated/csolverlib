76d27f04a799c5f8c03f00e6014c2bcaca417814
AG597 <connor.amico10@gmail.com>
2024-07-03 00:48:38 -0700

    Add files via upload

---
+++ hcap example.py
@@ -0,0 +1,61 @@
+import warnings
+from CSolver.hcap.solver import Solver
+import time
+from colorama import Fore
+import math
+import threading
+import ctypes
+import os
+import keyboard
+
+warnings.filterwarnings("ignore", message="Curlm already closed! quitting from process_data")
+warnings.filterwarnings("ignore", message="Curlm alread closed!", category=UserWarning)
+
+success_count = 0
+fail_count = 0
+total_attempts = 0
+total_time = 0
+lock = threading.Lock()
+stop_signal = False
+
+def update_console_title():
+    global success_count, fail_count, total_attempts
+    avg_solve_rate = (success_count / total_attempts) * 100 if total_attempts > 0 else 0
+    title = f"CSolver | {success_count}/{total_attempts} | {avg_solve_rate:.2f}%"
+    if os.name == 'nt':
+        ctypes.windll.kernel32.SetConsoleTitleW(title)
+
+def hCap():
+    global success_count, fail_count, total_attempts, total_time
+    while not stop_signal:
+        start = time.time()
+        cap = Solver(
+            sitekey='f5561ba9-8f1e-40ca-9b5b-a0b3f719ef34', # sitekey to match the url provided
+            site='discord.com', # Site the captcha is on
+            proxy="user:pass@ip:port" # Can be 'None' or ''
+        ).solve() # solve the captcha
+        end = time.time()
+        elapsed = end - start
+        e = round(elapsed, 2)
+
+        with lock:
+            total_attempts += 1
+            total_time += elapsed
+            if cap: # was the solve successful?
+                print(Fore.LIGHTCYAN_EX + f"Solved hCaptcha {Fore.YELLOW}[ {Fore.LIGHTBLUE_EX}{cap[:75]}***{Fore.YELLOW} ] {Fore.YELLOW}[{Fore.LIGHTBLUE_EX} {e}s{Fore.YELLOW} ]")
+                with open("solved.txt", 'a') as f: # writes solution to a file [ debugging purposes ] 
+                    f.write(f"{cap}\n")
+                return cap # returns the captcha solution
+            else:
+                fail_count += 1
+                print(Fore.LIGHTRED_EX + f"Failed to solve hCaptcha {Fore.YELLOW}[ {Fore.LIGHTBLUE_EX}{e}s {Fore.YELLOW}]")
+            update_console_title()
+            
+threads = []
+for i in range(1):
+    thread = threading.Thread(target=hCap)
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
---
+++ recap example.py
@@ -0,0 +1,41 @@
+from threading import Thread
+import time
+from colorama import Fore
+from CSolver.RecapV3.recap import Solver # import ReCap solver from CSolver 
+
+solver = Solver() # initialize solver
+
+def recap():
+    while True:
+        start = time.time()
+        cap = solver.solve( # Sovle the captcha
+            False, # Is the captcha invisible? 
+            "https://www.google.com/recaptcha/enterprise/anchor?ar=1&k=6LeMrv8ZAAAAAIcvo5HKvdj1lxtUdHnS13jUCulQ&co=aHR0cHM6Ly9wbGF5Lmhib21heC5jb206NDQz&hl=en&v=MuIyr8Ej74CrXhJDQy37RPBe&size=invisible&cb=f9q60qxahq1b", # The POST request to recaptcha [ also tell you if it is invisible or not ] 
+            "https://www.google.com/recaptcha/enterprise/reload?k=6LeMrv8ZAAAAAIcvo5HKvdj1lxtUdHnS13jUCulQ" # The recaptcha GET request
+        )
+        end = time.time()
+        elapsed = round(end - start, 2)
+
+        if cap:
+            print(Fore.LIGHTCYAN_EX + f"Solved ReCaptcha {Fore.YELLOW}[ {Fore.LIGHTBLUE_EX}{cap[:75]}***{Fore.YELLOW} ] {Fore.YELLOW}[{Fore.LIGHTBLUE_EX} {elapsed}s{Fore.YELLOW} ]")
+            with open("solved.txt", 'a') as f: # saves solutions to a txt [ For debugging purposes ]
+                f.write(f"{cap}\n")
+            return cap # returns the captcha solution 
+        else:
+            print(Fore.LIGHTRED_EX + f"Failed to solve ReCaptcha {Fore.YELLOW}[ {Fore.LIGHTBLUE_EX}{elapsed}s {Fore.YELLOW}]")
+            continue # retries in case of failure
+
+def solve(t):
+    threads = []
+    for _ in range(t):
+        thread = Thread(target=recap)
+        thread.start()
+        threads.append(thread)
+    
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    threads = 10 # how many captchas to solve at once
+    while True: # contonuously solve until closed
+        solve(threads)
